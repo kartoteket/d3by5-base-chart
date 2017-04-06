047ece2c351ecc92524422d3eec8ff566b1106e0
Svale Fossåskaret <svale@3by5.no>
2017-04-06 14:43:47 +0200

    new method: wrapText()

--- src/js/base-chart-utils.js
+++ src/js/base-chart-utils.js
@@ -323,6 +323,36 @@ function BaseUtils () {
       return formatter(value);
     },
 
+    // ref: https://bl.ocks.org/mbostock/7555321
+    wrapText: function(width, padding) {
+      return function() {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.3, // ems
+            x = text.attr('x') || 0,
+            y = text.attr("y") || 0,
+            dy = text.attr("dy") ? parseFloat(text.attr("dy")) : 0,
+            tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+        width = width || 120;
+        padding = padding || 5;
+        /*jshint -W084 */
+        while (word = words.pop()) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > (width - 2 * padding)) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+          }
+        }
+      };
+    },
+
+
   };
   return utils;
 }
